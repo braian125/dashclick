85ca940e73d89a8d4aa3bbea2ab99eede03f249b
Brian Vanegas <brianvanegas@viajesaeromar.com>
2020-01-09 22:56:08 -0500

    tooltip for nav icons

--- layout/ActionBar.vue
+++ layout/ActionBar.vue
@@ -7,7 +7,9 @@
             <ul>
                 <li v-for="(action, index) in actions" class="d-inline p-2">
                     <router-link :to="action.url" :key="index">
-                        <font-awesome-icon :icon="action.icon || 'rocket'"></font-awesome-icon>
+                        <el-tooltip class="item" effect="dark" :content="action.name" placement="bottom">
+                            <font-awesome-icon :icon="action.icon || 'rocket'"></font-awesome-icon>
+                        </el-tooltip>
                     </router-link>
                 </li>
             </ul>
