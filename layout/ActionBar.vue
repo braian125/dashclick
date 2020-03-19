981d878dd78ee682b602d639408973ad3a3ea8ad
Brian Vanegas <brianvanegas@viajesaeromar.com>
2020-03-19 09:48:44 -0500

    v-bind key for loop

--- layout/ActionBar.vue
+++ layout/ActionBar.vue
@@ -5,8 +5,8 @@
         </div>
         <div class="col">
             <ul>
-                <li v-for="(action, index) in actions" class="d-inline p-2">
-                    <router-link :to="action.url" :key="index">
+                <li v-for="(action, index) in actions" :key="index" class="d-inline p-2">
+                    <router-link :to="action.url">
                         <el-tooltip class="item" effect="dark" :content="action.name" placement="bottom">
                             <font-awesome-icon :icon="action.icon || 'rocket'"></font-awesome-icon>
                         </el-tooltip>
